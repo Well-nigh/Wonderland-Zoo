e6e56f2c12f17381934b9c7ab8966c9551b1997b
Artur <arturaxax@gmail.com>
2025-05-12 14:08:48 +0300

    Добавил проект

---
+++ Wonderland Zoo/js/script.js
@@ -0,0 +1,120 @@
+const x = document.querySelector("image1");
+const changes = document.getElementById("text1");
+x.addEventListener("mouseenter", function () {
+  changes.classList.add("sen");
+  x.classList.add("invis");
+});
+x.addEventListener("mouseleave", function () {
+  changes.classList.remove("sen");
+  x.classList.remove("invis");
+});
+const x2 = document.querySelector("image2");
+const changes2 = document.getElementById("text2");
+x2.addEventListener("mouseenter", function () {
+  changes2.classList.add("sen");
+  x2.classList.add("invis");
+});
+x2.addEventListener("mouseleave", function () {
+  changes2.classList.remove("sen");
+  x2.classList.remove("invis");
+});
+const x3 = document.querySelector("image3");
+const changes3 = document.getElementById("text3");
+x3.addEventListener("mouseenter", function () {
+  changes3.classList.add("sen");
+  x3.classList.add("invis");
+});
+x3.addEventListener("mouseleave", function () {
+  changes3.classList.remove("sen");
+  x3.classList.remove("invis");
+});
+const x4 = document.querySelector("image4");
+const changes4 = document.getElementById("text4");
+x4.addEventListener("mouseenter", function () {
+  changes4.classList.add("sen");
+  x4.classList.add("invis");
+});
+x4.addEventListener("mouseleave", function () {
+  changes4.classList.remove("sen");
+  x4.classList.remove("invis");
+});
+const x5 = document.querySelector("image5");
+const changes5 = document.getElementById("text5");
+x5.addEventListener("mouseenter", function () {
+  changes5.classList.add("sen");
+  x5.classList.add("invis");
+});
+x5.addEventListener("mouseleave", function () {
+  changes5.classList.remove("sen");
+  x5.classList.remove("invis");
+});
+const x6 = document.querySelector("image6");
+const changes6 = document.getElementById("text6");
+x6.addEventListener("mouseenter", function () {
+  changes6.classList.add("sen");
+  x6.classList.add("invis");
+});
+x6.addEventListener("mouseleave", function () {
+  changes6.classList.remove("sen");
+  x6.classList.remove("invis");
+});
+const x7 = document.querySelector("image7");
+const changes7 = document.getElementById("text7");
+x7.addEventListener("mouseenter", function () {
+  changes7.classList.add("sen");
+  x7.classList.add("invis");
+});
+x7.addEventListener("mouseleave", function () {
+  changes7.classList.remove("sen");
+  x7.classList.remove("invis");
+});
+const x8 = document.querySelector("image8");
+const changes8 = document.getElementById("text8");
+x8.addEventListener("mouseenter", function () {
+  changes8.classList.add("sen");
+  x8.classList.add("invis");
+});
+x8.addEventListener("mouseleave", function () {
+  changes8.classList.remove("sen");
+  x8.classList.remove("invis");
+});
+const x9 = document.querySelector("image9");
+const changes9 = document.getElementById("text9");
+x9.addEventListener("mouseenter", function () {
+  changes9.classList.add("sen");
+  x9.classList.add("invis");
+});
+x9.addEventListener("mouseleave", function () {
+  changes9.classList.remove("sen");
+  x9.classList.remove("invis");
+});
+const x10 = document.querySelector("image10");
+const changes10 = document.getElementById("text10");
+x10.addEventListener("mouseenter", function () {
+  changes10.classList.add("sen");
+  x10.classList.add("invis");
+});
+x10.addEventListener("mouseleave", function () {
+  changes10.classList.remove("sen");
+  x10.classList.remove("invis");
+});
+const x11 = document.querySelector("image11");
+const changes11 = document.getElementById("text11");
+x11.addEventListener("mouseenter", function () {
+  changes11.classList.add("sen");
+  x11.classList.add("invis");
+});
+x11.addEventListener("mouseleave", function () {
+  changes11.classList.remove("sen");
+  x11.classList.remove("invis");
+});
+const x12 = document.querySelector("image12");
+const changes12 = document.getElementById("text12");
+x12.addEventListener("mouseenter", function () {
+  changes12.classList.add("sen");
+  x12.classList.add("invis");
+});
+x12.addEventListener("mouseleave", function () {
+  changes12.classList.remove("sen");
+  x12.classList.remove("invis");
+});
